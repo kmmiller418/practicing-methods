c2089ef19f9d2c9710b283c6c33e0442e85d07eb
kmiller <kmmiller418@gmail.com>
2022-05-05 15:39:53 -0400

    Add MethodReturnTypes

---
+++ method-return-types/src/MethodReturnTypes.java
@@ -0,0 +1,16 @@
+public class MethodReturnTypes {
+    public static void main(String[] args) {
+        System.out.println(stringMeAlong());
+        System.out.println(intersNeverWin());
+        System.out.println(truesClues());
+    }
+    public static String stringMeAlong() {
+        return ("Not so fast, villain!");
+    }
+    public static int intersNeverWin() {
+        return (42);
+    }
+    public static boolean truesClues() {
+        return (true);
+    }
+}
